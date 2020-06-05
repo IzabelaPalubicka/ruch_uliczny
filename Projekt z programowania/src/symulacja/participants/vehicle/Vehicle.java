4a611423cbefcb60ce0d41c89dbf04b0ffc81404
IzabelaPalubicka <59491819+IzabelaPalubicka@users.noreply.github.com>
2020-06-05 16:07:29 +0200

    Update Vehicle.java

--- Projekt z programowania/src/symulacja/participants/vehicle/Vehicle.java
+++ Projekt z programowania/src/symulacja/participants/vehicle/Vehicle.java
@@ -2,20 +2,11 @@ package symulacja.participants.vehicle;
 
 import symulacja.mapa.PositionOccupation;
 
-public class Vehicle extends VehicleProperties implements VehicleFunction{
+public class Vehicle extends VehicleProperties implements VehicleFunction {
 
-    public Vehicle(int maxSpeed) {
-        super(maxSpeed);
-    }
 
-    @Override
-    public int speed() {
-        return 0;
-    }
-
-    @Override
-    public void move(int maxSpeed) throws InterruptedException {
-
+    public Vehicle(int maxSpeed, int speed) {
+        super(maxSpeed, speed);
     }
 
     public void accelerate(int speed, int maxSpeed) {
@@ -34,18 +25,18 @@ public class Vehicle extends VehicleProperties implements VehicleFunction{
         if (coordinateX == 1) {
             for (int i = 1; i == TIME_REACTION; i++)
                 for (int j = 0; j <= 4; j++) {
-                    if (Mapa[coordinateX + i][coordinateY - j] != positionOccupation.EMPTY) {
-                        return Mapa[coordinateX + i][coordinateY - j];
+                    if (Map[coordinateX + i][coordinateY - j] != positionOccupation.EMPTY) {
+                        return Map[coordinateX + i][coordinateY - j];
                     }
                     if (i == TIME_REACTION && j == 4) {
-                     return positionOccupation.EMPTY;
+                        return positionOccupation.EMPTY;
                     }
                 }
         } else {
             for (int i = 1; i == TIME_REACTION; i++)
                 for (int j = 0; j <= 4; j++) {
-                    if (Mapa[coordinateX - i][coordinateY + j] != positionOccupation.EMPTY) {
-                        return Mapa[coordinateX - i][coordinateY + j];
+                    if (Map[coordinateX - i][coordinateY + j] != positionOccupation.EMPTY) {
+                        return Map[coordinateX - i][coordinateY + j];
                     }
                     if (i == TIME_REACTION && j == 4) {
                         return positionOccupation.EMPTY;
@@ -53,6 +44,13 @@ public class Vehicle extends VehicleProperties implements VehicleFunction{
                 }
         }
 
-            return positionOccupation.EMPTY;
+        return positionOccupation.EMPTY;
     }
+
+    @Override
+    public void move(int maxSpeed, int coordinateX, int coordinateY) {
+
+    }
+
+
 }
