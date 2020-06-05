9ffe452c533eb6dda9b2ee6964102c66472d712d
agata20 <65370332+agata20@users.noreply.github.com>
2020-06-05 19:40:17 +0200

    Update Consola.java

--- Projekt z programowania/src/symulacja/main/Consola.java
+++ Projekt z programowania/src/symulacja/main/Consola.java
@@ -8,10 +8,7 @@ public interface Consola {
     public int setSafeSpeedBicycle();
     public int setNumberCar();
     public int setNumberBicycle();
-    public int setTimeReactionCar();
-    public int setTimeReactionBicycle();
     public int setNumberHuman();
     public int setNumberDog();
-
 
 }
