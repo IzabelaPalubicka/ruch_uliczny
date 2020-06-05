af005ea5454ed506d74ce19c0d34c15957ee8965
IzabelaPalubicka <59491819+IzabelaPalubicka@users.noreply.github.com>
2020-06-05 14:21:24 +0200

    Update Dog.java

--- Projekt z programowania/src/symulacja/participants/pedestrian/Dog.java
+++ Projekt z programowania/src/symulacja/participants/pedestrian/Dog.java
@@ -27,42 +27,16 @@ public class Dog extends APedestrian {
     }
 
     @Override
-    public void crossing(int firstCoordinateX, int firstCoordinateY,int coordinateY, int coordinateX, List<Integer> movingStory) throws InterruptedException {
-        super.crossing(firstCoordinateX, firstCoordinateY, coordinateY, coordinateX, movingStory);
+    public void crossing(int firstCoordinateY,int coordinateY, int coordinateX) {
+        super.crossing(firstCoordinateY, coordinateY, coordinateX);
     }
 
     @Override
-    public void leftPavementCrossing(int coordinateY, int coordinateX, List<Integer> movingStory) throws InterruptedException {
-        movingCoordinateY = coordinateY;
-        for (int i = 1; i < 6; i++) {
-            if (movingCoordinateY == 5)
-                System.gc();
-            else {
-                movingCoordinateY++;
-                movingStory.add(coordinateY);
-                mapa[coordinateX][movingStory.get(movingCoordinateY) - 1] = positionOccupation.EMPTY;
-                mapa[coordinateX][movingCoordinateY++] = positionOccupation.DOG;
-                Thread.sleep(3000);
-            }
-        }
+    public void leftPavementCrossing(int coordinateY, int coordinateX) {
     }
 
     @Override
-    public void rightPavementCrossing(int coordinateY, int coordinateX, List<Integer> movingStory) throws InterruptedException {
-        movingCoordinateY = coordinateY;
-        for (int i = 1; i < 6; i++) {
-            if (movingCoordinateY == 0)
-                System.gc();
-            else {
-                movingCoordinateY--;
-                movingStory.add(coordinateY);
-                mapa[coordinateX][movingStory.get(movingCoordinateY) + 1] = positionOccupation.EMPTY;
-                mapa[coordinateX][movingCoordinateY++] = positionOccupation.DOG;
-                Thread.sleep(3000);
-            }
-        }
-
-
+    public void rightPavementCrossing(int coordinateY, int coordinateX){
     }
 
 }
