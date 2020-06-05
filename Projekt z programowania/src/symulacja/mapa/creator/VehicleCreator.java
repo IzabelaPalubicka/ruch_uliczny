64382d7d7418e627f82055afa1a10ee0903678b3
IzabelaPalubicka <59491819+IzabelaPalubicka@users.noreply.github.com>
2020-06-05 21:21:34 +0200

    Update VehicleCreator.java

--- Projekt z programowania/src/symulacja/mapa/creator/VehicleCreator.java
+++ Projekt z programowania/src/symulacja/mapa/creator/VehicleCreator.java
@@ -2,13 +2,15 @@ package symulacja.mapa.creator;
 
 
 import symulacja.mapa.Mapa;
-import symulacja.participants.vehicle.VehicleFunction;
+import symulacja.participants.vehicle.Bicycle;
+import symulacja.participants.vehicle.Car;
+
 
 import java.util.List;
 
 public interface VehicleCreator {
 
-    public List<VehicleFunction> creatorCars(Mapa mapa, int maxSpeed, int speed);
-    public List<VehicleFunction> creatorBicycles(Mapa mapa, int maxSpeed, int speed);
+    public List<Car> creatorCars(Mapa mapa, int maxSpeed, int speed);
+    public List<Bicycle> creatorBicycles(Mapa mapa, int maxSpeed, int speed);
 
 }
