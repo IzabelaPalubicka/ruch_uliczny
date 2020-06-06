9665dcb88aae0bc7d9296f63df625d5caf549957
IzabelaPalubicka <59491819+IzabelaPalubicka@users.noreply.github.com>
2020-06-06 14:02:53 +0200

    Update APedestrian.java

--- Projekt z programowania/src/symulacja/participants/pedestrian/APedestrian.java
+++ Projekt z programowania/src/symulacja/participants/pedestrian/APedestrian.java
@@ -1,61 +1,70 @@
 package symulacja.participants.pedestrian;
 
+import symulacja.mapa.MapSimple;
 import symulacja.mapa.PositionOccupation;
 
-import java.util.List;
+import java.util.Random;
+
 
 public abstract class APedestrian implements Pedestrian {
 
-    protected int coordinateX;
-    protected int coordinateY;
-    protected int firstCoordinateY;
+    public int coordinateX;
+    public int coordinateY;
+    public int firstCoordinateY;
     protected PositionOccupation positionOccupation;
-    protected PositionOccupation mapa[][];
-
+   // protected PositionOccupation mapa[][];
+    //Random random = new Random();
     public APedestrian() {
         this.coordinateX = coordinateX();
         this.coordinateY = coordinateY();
         this.firstCoordinateY = coordinateY;
-        this.positionOccupation = positionOccupation;
-       
+   //     this.positionOccupation = positionOccupation;
     }
 
     @Override
-    public void crossing(int firstCoordinateY, int coordinateY, int coordinateX) {
+    public void crossing(int firstCoordinateY, int coordinateY, int coordinateX, MapSimple position) {
         if (firstCoordinateY == 0)
-            rightPavementCrossing(coordinateY, coordinateX);
+            rightPavementCrossing(coordinateY, coordinateX, position);
         else if (firstCoordinateY == 5)
-            leftPavementCrossing(coordinateY, coordinateX);
+            leftPavementCrossing(coordinateY, coordinateX, position);
     }
 
     @Override
-    public void leftPavementCrossing(int coordinateY, int coordinateX) {
+    public void leftPavementCrossing(int coordinateY, int coordinateX, MapSimple position) {
         if (coordinateY == 5)
             System.gc();
         else {
-            mapa[coordinateX][coordinateY] = positionOccupation.EMPTY;
-            mapa[coordinateX][coordinateY++] = positionOccupation.HUMAN;
+            position.mapa[coordinateX][coordinateY] = positionOccupation.EMPTY;
+            position.mapa[coordinateX][++coordinateY] = positionOccupation.HUMAN;
         }
     }
 
     @Override
-    public void rightPavementCrossing(int coordinateY, int coordinateX) {
+    public void rightPavementCrossing(int coordinateY, int coordinateX, MapSimple position) {
         if (coordinateY == 0)
             System.gc();
         else {
-            mapa[coordinateX][coordinateY] = positionOccupation.EMPTY;
-            mapa[coordinateX][coordinateY--] = positionOccupation.DOG;
+            position.mapa[coordinateX][coordinateY] = positionOccupation.EMPTY;
+            position.mapa[coordinateX][--coordinateY] = positionOccupation.DOG;
         }
     }
 
 
     @Override
     public int coordinateX() {
+        Random random = new Random();
+        do {
+            coordinateX = random.nextInt() % 32;
+        }while(coordinateX < 0);
         return coordinateX;
     }
 
     @Override
     public int coordinateY() {
+        Random random = new Random();
+         do {
+            coordinateY = random.nextInt() % 6;
+        }while (coordinateY != 0 && coordinateY != 5);
         return coordinateY;
     }
 
