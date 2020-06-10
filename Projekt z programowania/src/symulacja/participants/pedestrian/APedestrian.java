480f7a907085cee660c699fa3996a613894d7e06
IzabelaPalubicka <59491819+IzabelaPalubicka@users.noreply.github.com>
2020-06-10 17:25:09 +0200

    Update APedestrian.java

--- Projekt z programowania/src/symulacja/participants/pedestrian/APedestrian.java
+++ Projekt z programowania/src/symulacja/participants/pedestrian/APedestrian.java
@@ -12,59 +12,34 @@ public abstract class APedestrian implements Pedestrian {
     public int coordinateY;
     public int firstCoordinateY;
     protected PositionOccupation positionOccupation;
-   // protected PositionOccupation mapa[][];
-    //Random random = new Random();
+
     public APedestrian() {
         this.coordinateX = coordinateX();
         this.coordinateY = coordinateY();
         this.firstCoordinateY = coordinateY;
-   //     this.positionOccupation = positionOccupation;
     }
 
     @Override
-    public void crossing(int firstCoordinateY, int coordinateY, int coordinateX, MapSimple position) {
-        if (firstCoordinateY == 0)
-            rightPavementCrossing(coordinateY, coordinateX, position);
-        else if (firstCoordinateY == 5)
-            leftPavementCrossing(coordinateY, coordinateX, position);
+    public int crossing(int firstCoordinateY, int coordinateY, int coordinateX, PositionOccupation[][] mapa) {
+        return coordinateY;
     }
+
 
     @Override
-    public void leftPavementCrossing(int coordinateY, int coordinateX, MapSimple position) {
-        if (coordinateY == 5)
-            System.gc();
-        else {
-            position.mapa[coordinateX][coordinateY] = positionOccupation.EMPTY;
-            position.mapa[coordinateX][++coordinateY] = positionOccupation.HUMAN;
-        }
-    }
-
-    @Override
-    public void rightPavementCrossing(int coordinateY, int coordinateX, MapSimple position) {
-        if (coordinateY == 0)
-            System.gc();
-        else {
-            position.mapa[coordinateX][coordinateY] = positionOccupation.EMPTY;
-            position.mapa[coordinateX][--coordinateY] = positionOccupation.DOG;
-        }
-    }
-
-
-  @Override
     public int coordinateX() {
         Random random = new Random();
         do {
             coordinateX = random.nextInt() % 32;
-        }while(coordinateX < 0);
+        } while (coordinateX < 0);
         return coordinateX;
     }
 
     @Override
     public int coordinateY() {
         Random random = new Random();
-         do {
+        do {
             coordinateY = random.nextInt() % 6;
-        }while (coordinateY != 0 && coordinateY != 5);
+        } while (coordinateY != 0 && coordinateY != 5);
         return coordinateY;
     }
 
