30d62165c091a0dcd452fd5314fdc72188fd8222
IzabelaPalubicka <59491819+IzabelaPalubicka@users.noreply.github.com>
2020-06-10 17:27:32 +0200

    Update Human.java

--- Projekt z programowania/src/symulacja/participants/pedestrian/Human.java
+++ Projekt z programowania/src/symulacja/participants/pedestrian/Human.java
@@ -8,25 +8,17 @@ public class Human extends APedestrian {
 
     public Human() {
     }
+    
+@Override
+    public int crossing(int firstCoordinateY, int coordinateY, int coordinateX, PositionOccupation[][] mapa) {
+        if (firstCoordinateY == 0) {
+            coordinateY = coordinateY + 1;
+        } else if (firstCoordinateY == 5) {
 
-    @Override
-    public void crossing(int firstCoordinateY, int coordinateY, int coordinateX, MapSimple position) {
+            coordinateY = coordinateY - 1;
+        }
+        return coordinateY;
     }
-
-
-//    public int coordinateX() {
-//      //  Random random = new Random();
-////        while (coordinateX != 2 && coordinateX != 8 && coordinateX != 11 && coordinateX != 12 && coordinateX != 13 && coordinateX != 18
-////                && coordinateX != 19 && coordinateX != 20 && coordinateX != 25 && coordinateX != 30) {
-////            coordinateX = random.nextInt() % 32;
-////        }
-//        return coordinateX;
-//    }
-
-//    public int coordinateY() {
-//
-//        return coordinateY;
-//    }
 
 
     @Override
@@ -37,15 +29,6 @@ public class Human extends APedestrian {
     @Override
     public int coordinateY() {
         return super.coordinateY();
-    }
-
-    @Override
-    public void leftPavementCrossing(int coordinateY, int coordinateX, MapSimple position) {
-    }
-
-
-    @Override
-    public void rightPavementCrossing(int coordinateY, int coordinateX, MapSimple position) {
     }
 
 
