eb0f008b4bdbeac29eee48eb60e112d8d2ff72e9
IzabelaPalubicka <59491819+IzabelaPalubicka@users.noreply.github.com>
2020-06-10 21:02:44 +0200

    Update Pedestrian.java

--- Projekt z programowania/src/symulacja/participants/pedestrian/Pedestrian.java
+++ Projekt z programowania/src/symulacja/participants/pedestrian/Pedestrian.java
@@ -1,14 +1,9 @@
 package symulacja.participants.pedestrian;
 
+import symulacja.mapa.PositionOccupation;
 import symulacja.participants.RandomPositionOccupation;
-
-import java.util.List;
 
 public interface Pedestrian extends RandomPositionOccupation {
 
-    void crossing(int firstCoordinateY, int coordinateY, int coordinateX);
-
-    void leftPavementCrossing(int coordinateY, int coordinateX);
-
-    void rightPavementCrossing(int coordinateY, int coordinateX);
+    int crossing(int firstCoordinateY, int coordinateY, int coordinateX, PositionOccupation[][] mapa);
 }
