15236647500b5e8fcf52bcd1ebac7c20efef5c7f
agata20 <65370332+agata20@users.noreply.github.com>
2020-06-11 04:12:13 +0200

    Update Human.java

--- Projekt z programowania/src/symulacja/participants/pedestrian/Human.java
+++ Projekt z programowania/src/symulacja/participants/pedestrian/Human.java
@@ -10,23 +10,9 @@ public class Human extends APedestrian {
     }
 
     @Override
-    public void crossing(int firstCoordinateY, int coordinateY, int coordinateX, MapSimple position) {
+    public int crossing(int n, int firstCoordinateY, int coordinateY, int coordinateX) {
+        return 0;
     }
-
-
-//    public int coordinateX() {
-//      //  Random random = new Random();
-////        while (coordinateX != 2 && coordinateX != 8 && coordinateX != 11 && coordinateX != 12 && coordinateX != 13 && coordinateX != 18
-////                && coordinateX != 19 && coordinateX != 20 && coordinateX != 25 && coordinateX != 30) {
-////            coordinateX = random.nextInt() % 32;
-////        }
-//        return coordinateX;
-//    }
-
-//    public int coordinateY() {
-//
-//        return coordinateY;
-//    }
 
 
     @Override
@@ -38,16 +24,6 @@ public class Human extends APedestrian {
     public int coordinateY() {
         return super.coordinateY();
     }
-
-    @Override
-    public void leftPavementCrossing(int coordinateY, int coordinateX, MapSimple position) {
-    }
-
-
-    @Override
-    public void rightPavementCrossing(int coordinateY, int coordinateX, MapSimple position) {
-    }
-
 
 }
 
